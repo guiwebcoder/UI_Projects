6cb6f5a73506a8de07c1664bcd3fe92ae9defa4e
guiwebcoder <64625989+guiwebcoder@users.noreply.github.com>
2020-08-19 19:53:22 +0530

    Add files via upload

---
+++ logistic-backend-master/include/nav.php
@@ -0,0 +1,20 @@
+<nav class="justify-content-md-between">
+  <div class="d-none d-sm-block float-left back-btn" id="">
+    &larr; Catalogue
+  </div>
+  <ul class="nav-items">
+    <li class="nav-item">
+      <i class="fa fa-bell icon"></i>
+      <i class="fa fa-circle dot"></i>
+    </li>
+    <li class="nav-item">
+      <p class="user-name">
+        Michael McClellan
+        <i class="fa fa-angle-down"></i>
+      </p>
+      <div class="user-icon">
+        <img src="static/assets/user.png" alt="user profile image" />
+      </div>
+    </li>
+  </ul>
+</nav>
---
+++ logistic-backend-master/include/sidebar.php
@@ -0,0 +1,66 @@
+<div class="sidebar-wrapper" id="sidebar-panel">
+  <div class="brand">
+    <div class="logo">
+      <img src="static/assets/logo.svg" alt="logo" />
+    </div>
+  </div>
+  <p class="sidebar-head divider">Main</p>
+  <ul>
+    <a href="index.html">
+      <li class="sidebar-item"> <i class="fa fa-list-alt icon"></i>Dashboard</li>
+    </a>
+    <a href="order.html">
+      <li class="sidebar-item">
+        <img class="icon" src="static/assets/icons/gift.svg" alt="" />Orders
+      </li>
+    </a>
+    <a href="">
+      <li class="sidebar-item">
+        <img class="icon" src="static/assets/icons/trending.svg" alt="" />Analytics
+      </li>
+    </a>
+    <a href="">
+      <li class="sidebar-item"> <i class="fa fa-puzzle-piece icon"></i>Catalogue</li>
+    </a>
+    <a href="">
+      <li class="sidebar-item"> <i class="fa fa-sitemap icon"></i>Payment</li>
+    </a>
+    <a href="">
+      <li class="sidebar-item"> <i class="fa fa-users icon"></i>Discount</li>
+    </a>
+    <a href="store-keeper.html">
+      <li class="sidebar-item"> <i class="fa fa-question-circle icon"></i>Employees</li>
+    </a>
+    <a href="customers.html">
+      <li class="sidebar-item"> <i class="fa fa-tags icon"></i>Customers</li>
+    </a>
+  </ul>
+  <p class="sidebar-head divider">Sales Channels</p>
+  <ul>
+    <a href="">
+      <li class="sidebar-item"> <i class="fa fa-percent icon"></i>Online Store</li>
+    </a>
+    <a href="/merchant/activity">
+      <li class="sidebar-item">
+        <img class="icon" src="static/assets/icons/mail.svg" alt="" />Marketing
+      </li>
+    </a>
+    <a href="/merchant/setting">
+      <li class="sidebar-item"> <i class="fa fa-cog icon"></i>Configuration</li>
+    </a>
+  </ul>
+  <p class="sidebar-head divider">Configuration</p>
+  <ul>
+    <a href="">
+      <li class="sidebar-item"> <i class="fa fa-percent icon"></i>Project Deatils</li>
+    </a>
+    <a href="/merchant/activity">
+      <li class="sidebar-item"> <i class="fa fa-list-alt icon"></i>Team</li>
+    </a>
+    <a href="/merchant/setting">
+      <li class="sidebar-item">
+        <img class="icon" src="static/assets/icons/support.svg" alt="" />Support
+      </li>
+    </a>
+  </ul>
+</div>
